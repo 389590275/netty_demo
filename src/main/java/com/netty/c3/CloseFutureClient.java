9667b530ca78d4f3a301448b2f5d78f7be2d593e
xiangchijie <xiangchijie@bixin.cn>
2021-12-08 19:19:15 +0800

    提交

--- src/main/java/com/netty/c3/CloseFutureClient.java
+++ src/main/java/com/netty/c3/CloseFutureClient.java
@@ -3,10 +3,13 @@ package com.netty.c3;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -22,13 +25,15 @@ public class CloseFutureClient {
 
 
     public static void main(String[] args) throws InterruptedException, IOException {
+        NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup();
         // 带有Future Promise 的类型都是和异步方法配套使用，用来处理结果
         ChannelFuture channelFuture = new Bootstrap()
-                .group(new NioEventLoopGroup())
+                .group(eventLoopGroup)
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override // 在建立连接后调用
                     protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                         // String转成ByteBuf
                         ch.pipeline().addLast(new StringEncoder());
 
@@ -50,6 +55,17 @@ public class CloseFutureClient {
                 channel.writeAndFlush(line);
             }
         }, "input").start();
+
+        // 获取 ChannelFuture 对象 1)同步处理关闭  2）异步处理关闭
+        ChannelFuture closeFuture = channel.closeFuture();
+        //1. channelFuture.sync();
+        //2.addListener
+        closeFuture.addListener((ChannelFutureListener) future -> {
+            log.debug("channelFuture operationComplete channel:{}", future.channel());
+            // 优雅结束 拒绝接受新的任务，现有任务执行完 关闭线程
+            eventLoopGroup.shutdownGracefully();
+        });
+
     }
 
 
--- src/main/java/com/netty/c3/TestJdkFuture.java
+++ src/main/java/com/netty/c3/TestJdkFuture.java
@@ -1,6 +1,5 @@
 package com.netty.c3;
 
-import io.netty.util.NettyRuntime;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Callable;
@@ -17,15 +16,12 @@ public class TestJdkFuture {
 
     public static void main(String[] args) throws Exception {
         // 1. 线程池
-        ExecutorService service = Executors.newFixedThreadPool(2);
+        ExecutorService service = Executors.newSingleThreadExecutor();
         // 2. 提交任务
-        Future<Integer> future = service.submit(new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                log.debug("执行计算");
-                Thread.sleep(1000);
-                return 50;
-            }
+        Future<Integer> future = service.submit(() -> {
+            log.debug("执行计算");
+            Thread.sleep(1000);
+            return 50;
         });
         // 3. 主线程通过future来获取结果
         log.debug("等待结果");
